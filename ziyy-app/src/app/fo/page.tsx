74c8f9c82b81c211f9ebe458b6033f2ba8673349
Edesius Jaty Giovanni <edesiusjatyg@gmail.com>
2025-06-11 01:16:02 +0700

    initial marketing page

--- ziyy-app/src/app/fo/page.tsx
+++ ziyy-app/src/app/fo/page.tsx
@@ -1,11 +1,11 @@
-import { CirclePlus } from "lucide-react";
-import { Card } from "@/components/ui/card";
+import { CirclePlus, ChevronsRight } from "lucide-react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
 
 export default function Page() {
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-300 to-slate-500 p-8 flex items-center justify-center">
-            <div className="w-[90%]">
+            <div className="w-[70%]">
                 <div className="bg-white/80 backdrop-blur-md rounded-2xl shadow-2xl">
                     <div className="flex items-center justify-between bg-slate-500/50 rounded-t-2xl pb-4 pt-4 px-6">
                         <h2 className="text-black font-semibold text-xl">Ziyy Gym | Front Office</h2>
@@ -27,62 +27,109 @@ export default function Page() {
                     <p className="text-black font-semibold text-xl pl-6 pt-6">Rabu, 11 Juni 2025</p>
 
                     <div className="grid grid-cols-5 gap-6 p-6">
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Total Member</h2>
-                                <h3 className="text-xl font-semibold">200</h3>
-                            </div>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Member</CardTitle>
+                                <CardDescription>Total Member</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">178</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Member Baru</h2>
-                                <h3 className="text-xl font-semibold">9</h3>
-                            </div>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Member</CardTitle>
+                                <CardDescription>Member Baru</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">9</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Member Mendekati Habis</h2>
-                                <h3 className="text-xl font-semibold">23</h3>
-                            </div>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Member</CardTitle>
+                                <CardDescription>Mendekati Habis</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">23</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Pemasukan Hari ini</h2>
-                                <h3 className="text-xl font-semibold">Rp2.400.000</h3>
-                            </div>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Pemasukan</CardTitle>
+                                <CardDescription>Hari ini</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">Rp2.400.000</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Pengeluaran Hari ini</h2>
-                                <h3 className="text-xl font-semibold">Rp400.000</h3>
-                            </div>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Pengeluaran</CardTitle>
+                                <CardDescription>Hari ini</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">Rp400.000</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
                         </Card>
                     </div>
 
                     <div className="grid grid-cols-3 gap-6 pl-6 pr-6 pb-6">
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Absensi Member</h2>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Absensi Member</CardTitle>
+                            </CardHeader>
+                            <CardContent></CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500 gap-2">
                                 <CirclePlus />
-                            </div>
+                                <p className="text-md font-semibold">Tambah</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Tambah Member</h2>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Tambah Member</CardTitle>
+                            </CardHeader>
+                            <CardContent></CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500 gap-2">
                                 <CirclePlus />
-                            </div>
+                                <p className="text-md font-semibold">Tambah</p>
+                            </CardFooter>
                         </Card>
 
-                        <Card className="flex bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <div className="flex flex-col items-left justify-center space-y-4">
-                                <h2 className="text-xl font-semibold">Tambah Transaksi</h2>
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Tambah Transaksi</CardTitle>
+                            </CardHeader>
+                            <CardContent></CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500 gap-2">
                                 <CirclePlus />
-                            </div>
+                                <p className="text-md font-semibold">Tambah</p>
+                            </CardFooter>
                         </Card>
                     </div>
                 </div>
--- ziyy-app/src/app/mkt/page.tsx
+++ ziyy-app/src/app/mkt/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
             <div className="w-[70%]">
                 <div className="bg-white/80 backdrop-blur-md rounded-2xl shadow-2xl">
                     <div className="flex items-center justify-between bg-slate-500/50 rounded-t-2xl pb-4 pt-4 px-6">
-                        <h2 className="text-black font-semibold text-xl">Ziyy Gym | Accounting</h2>
+                        <h2 className="text-black font-semibold text-xl">Ziyy Gym | Marketing</h2>
                         <Breadcrumb>
                             <BreadcrumbList>
                                 <BreadcrumbItem>
@@ -17,7 +17,7 @@ export default function Page() {
                                     </BreadcrumbLink>
                                     <BreadcrumbSeparator></BreadcrumbSeparator>
                                     <BreadcrumbPage>
-                                        Accounting
+                                        Marketing
                                     </BreadcrumbPage>
                                 </BreadcrumbItem>
                             </BreadcrumbList>
@@ -26,79 +26,39 @@ export default function Page() {
 
                     <p className="text-black font-semibold text-xl pl-6 pt-6">Rabu, 11 Juni 2025</p>
 
-                    <div className="grid grid-cols-5 gap-6 p-6">
+                    <div className="grid grid-cols-2 gap-6 p-6">
                         <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
                             <CardHeader>
-                                <CardTitle>Pemasukan</CardTitle>
+                                <CardTitle>Campaign Aktif</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg font-semibold">2</p>
+                            </CardContent>
+                            <CardFooter className="flex-row hover:text-gray-500">
+                                <ChevronsRight />
+                                <p className="text-md font-semibold">Detail</p>
+                            </CardFooter>
+                        </Card>
+
+                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
+                            <CardHeader>
+                                <CardTitle>Total Aktivitas</CardTitle>
                                 <CardDescription>Hari ini</CardDescription>
                             </CardHeader>
                             <CardContent>
-                                <p className="text-lg font-semibold">Rp2.400.000</p>
+                                <p className="text-lg font-semibold">3</p>
                             </CardContent>
                             <CardFooter className="flex-row hover:text-gray-500">
                                 <ChevronsRight />
                                 <p className="text-md font-semibold">Detail</p>
-                            </CardFooter>
-                        </Card>
-
-                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <CardHeader>
-                                <CardTitle>Pengeluaran</CardTitle>
-                                <CardDescription>Hari ini</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-lg font-semibold">Rp400.000</p>
-                            </CardContent>
-                            <CardFooter className="flex-row hover:text-gray-500">
-                                <ChevronsRight />
-                                <p className="text-md font-semibold">Detail</p>
-                            </CardFooter>
-                        </Card>
-
-                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <CardHeader>
-                                <CardTitle>Pemasukan</CardTitle>
-                                <CardDescription>Bulan ini</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-lg font-semibold">Rp12.800.000</p>
-                            </CardContent>
-                            <CardFooter className="flex-row hover:text-gray-500">
-                                <ChevronsRight />
-                                <p className="text-md font-semibold">Detail</p>
-                            </CardFooter>
-                        </Card>
-
-                        <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
-                            <CardHeader>
-                                <CardTitle>Pengeluaran</CardTitle>
-                                <CardDescription>Bulan ini</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-lg font-semibold">Rp3.700.000</p>
-                            </CardContent>
-                            <CardFooter className="flex-row hover:text-gray-500">
-                                <ChevronsRight />
-                                <p className="text-md font-semibold">Detail</p>
-                            </CardFooter>
-                        </Card>
-
-                        <Card className="flex flex-col justify-between bg-white p-6">
-                            <CardHeader>
-                                <CardTitle>Total Kas</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-lg font-semibold">Rp22.400.000</p>
-                            </CardContent>
-                            <CardFooter className="flex-row hover:text-gray-500">
                             </CardFooter>
                         </Card>
                     </div>
 
-                    <div className="grid grid-cols-2 gap-6 pt-6 pb-6 pl-88 pr-88">
+                    <div className="grid grid-cols-2 gap-6 pl-6 pr-6 pb-6 pt-0">
                         <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
                             <CardHeader>
-                                <CardTitle>Daftar Transaksi</CardTitle>
+                                <CardTitle>Daftar Campaign</CardTitle>
                             </CardHeader>
                             <CardContent></CardContent>
                             <CardFooter className="flex-row hover:text-gray-500">
@@ -109,7 +69,7 @@ export default function Page() {
 
                         <Card className="flex flex-col justify-between bg-white hover:bg-white/70 p-6 cursor-pointer">
                             <CardHeader>
-                                <CardTitle>Tambah Transaksi</CardTitle>
+                                <CardTitle>Catat Aktivitas</CardTitle>
                             </CardHeader>
                             <CardContent></CardContent>
                             <CardFooter className="flex-row hover:text-gray-500 gap-2">
