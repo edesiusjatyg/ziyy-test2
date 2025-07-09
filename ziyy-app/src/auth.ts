428167a7a2ca67349d4e2e46b93feabf5b0bccb2
Edesius Jaty Giovanni <edesiusjatyg@gmail.com>
2025-07-09 23:16:34 +0700

    fix(auth): resolve auth module error

--- ziyy-app/src/auth.ts
+++ ziyy-app/src/auth.ts
@@ -1,35 +1,24 @@
-// import NextAuth from "next-auth"
-// import Credentials from "next-auth/providers/credentials"
-// import { PrismaAdapter } from "@auth/prisma-adapter"
-// import { prisma } from "prisma"
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   providers: [
-//     Credentials({
-//       credentials: {
-//         username: {},
-//         password: {},
-//       },
-//       authorize: async (credentials) => {
-//         // const user = await getUserFromDB(credentials.username);
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        username: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const user = await (credentials.username);
 
-//         // if (!user) {
-//         //   throw new Error("Invalid credentials.");
-//         // }
+        if (!user) {
+          throw new Error("Invalid credentials.");
+        }
 
-//         // const isPasswordValid = await compare(
-//         //   credentials.password,
-//         //   user.password
-//         // );
-
-//         // if (!isPasswordValid) {
-//         //   throw new Error("Invalid credentials.");
-//         // }
-
-//         // const { password, ...userWithoutPassword } = user;
-//         // return userWithoutPassword;
-//       },
-//     }),
-//   ],
-// });
+        return user;
+      },
+    }),
+  ],
+});
